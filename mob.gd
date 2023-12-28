94b49ce61ac2c41df0e18c21e61a90689623703a
Kingsmai <xsbugh@gmail.com>
2023-12-29 00:51:03 +0800

    Slime can now be kill by the player projectile

--- mob.gd
+++ mob.gd
@@ -1,13 +1,22 @@
 extends CharacterBody2D
 
+var health = 3
 
 @onready var player : CharacterBody2D = $/root/Game/Player
+
+
+func _ready() -> void:
+	%Slime.play_walk()
 
 
 func _physics_process(delta: float) -> void:
 	var direction = global_position.direction_to(player.global_position)
 	velocity = direction * 150.0
 	move_and_slide()
-	
-	if velocity.length() > 0.0:
-		%Slime.play_walk()
+
+
+func take_damage():
+	health -= 1
+	%Slime.play_hurt()
+	if health == 0:
+		queue_free()
