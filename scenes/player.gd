d9486ac0449182408a87ec3276af9f722953a878
Kingsmai <xsbugh@gmail.com>
2023-12-29 01:50:07 +0800

    Added player health system

--- scenes/player.gd
+++ scenes/player.gd
@@ -1,5 +1,16 @@
 extends CharacterBody2D
 
+signal health_depleted
+
+var health := 100.0
+const DAMAGE_RATE := 5.0
+
+@onready var health_bar: ProgressBar = $HealthBar
+
+
+func _ready() -> void:
+	health_bar.max_value = health
+	health_bar.value = health
 
 func _physics_process(delta: float) -> void:
 	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
@@ -10,3 +21,11 @@ func _physics_process(delta: float) -> void:
 		%HappyBoo.play_walk_animation()
 	else:
 		%HappyBoo.play_idle_animation()
+	
+	var overlapping_mobs = (%HurtBox as Area2D).get_overlapping_bodies()
+	if overlapping_mobs.size() > 0:
+		health -= DAMAGE_RATE * overlapping_mobs.size() * delta
+		health_bar.value = health
+		if health <= 0.0:
+			# Game over
+			health_depleted.emit()
